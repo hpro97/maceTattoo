6a81115875959cd2617bebdde153302c3c145614
Hpro97 <haxmo25@gmail.com>
2024-04-16 19:51:17 +0100

    Fix gallery layout

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import MaceTattooing from "../../public/maceTattooing.jpg";
+
 import AboutMe from "../Components/AboutMe";
 import Testimonials from "../Components/Testimonials";
 import FAQs from "../Components/FAQs";
@@ -6,22 +6,6 @@ import FAQs from "../Components/FAQs";
 const Home = () => {
   return (
     <>
-      <div className="imgContainer">
-        <img
-          src={MaceTattooing}
-          alt="Mace Tattooing black and grey candid shot"
-          style={{
-            width: "100%",
-            maxWidth: "100%",
-            height: "100%",
-            maxHeight: "100%",
-          }}
-        />
-      </div>
-      <div className="headerContainer">
-        <h1 className="landingHeader">Mace Tattoo</h1>
-      </div>
-      <hr />
       <AboutMe />
       <Testimonials />
       <FAQs />
